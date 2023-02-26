4d35e6005c94e900aac46aab73d94dd115d93624
HaLan0101 <dnhalan0101@gmail.com>
2023-02-26 23:59:39 +0100

    save code

--- src/pages/place/[id]/index.js
+++ src/pages/place/[id]/index.js
@@ -13,11 +13,11 @@ import Door from "../../../../public/door.svg";
 import Button from "../../../components/Button";
 import Input from "../../../components/Input";
 const Index = () => {
-  const [heart, setHeart] = useState(HeartIcon.src);
+  // const [heart, setHeart] = useState(HeartIcon.src);
   const router = useRouter();
   const [place, setPlace] = useState();
-  const { addPlaceWishlist } = useContext(WishlistContext);
-  const { removePlaceWishlist } = useContext(WishlistContext);
+  // const { addPlaceWishlist } = useContext(WishlistContext);
+  // const { removePlaceWishlist } = useContext(WishlistContext);
 
   useEffect(() => {
     if (router.isReady) {
@@ -27,107 +27,110 @@ const Index = () => {
   }
   }, [router.isReady])
 
-  const toggleHeart = () =>{
-    if(heart === HeartIcon.src){
-      setHeart(HeartIconClick.src)
-    }
-    else{
-      setHeart(HeartIcon.src)
-    }
-  }
+  // const toggleHeart = () =>{
+  //   if(heart === HeartIcon.src){
+  //     setHeart(HeartIconClick.src)
+  //   }
+  //   else{
+  //     setHeart(HeartIcon.src)
+  //   }
+  // }
 
   return (
     place && (
       <>
       <div className={styles.product__detail}>
         <div className={styles.product__header}>
-            <h1>{place.title}</h1>
+            <h1>{place.place.title}</h1>
             <div className={styles.product__header__info}>
               <ul className={styles.nav__list}>
                 <li className={styles.nav__item}>
                   <img src={Star.src} alt="star" />
-                  <p>{place.rating}</p>
+                  <p className={styles.nav__decor}>{place.place.rating}</p>
                 </li>
-                <li className={styles.nav__item}>19 commentaires</li>
+                <li className={styles.nav__item}><p className={styles.nav__decor}>19 commentaire</p></li>
                 <li className={styles.nav__item}>
                   <img src={Medal.src} alt="medal" />
                   <p>Superhôte</p>
                 </li>
-                <li className={styles.nav__item}>{place.address.street}, {place.address.zipCode} {place.address.city}</li>
+                <li className={styles.nav__item}><p className={styles.nav__decor}>{place.place.address.street}, {place.place.address.zipCode} {place.place.address.city}</p></li>
               </ul>
               <ul className={styles.nav__list}>
                 <li className={styles.nav__item}>
                   <img src={Share.src} alt="share" />
-                  <p>Partager</p>
+                  <p className={styles.nav__decor}>Partager</p>
                 </li>
                 <li className={styles.nav__item}>
-                  <button
-                    className={styles.btn__whishlist}
-                    onClick={
-                      () => {
-                        addPlaceWishlist(place);
-                        removePlaceWishlist(place);
-                        toggleHeart()
-                      }
-                    }
-                  >
-                    <img src={heart} alt="favoris" />
-                  </button>
-                  <p>Entregistrer</p>
+                    <img src={HeartIconClick.src} alt="favoris" />
+                    <p className={styles.nav__decor}>Entregistrer</p>
                 </li>
               </ul>
             </div>
-            {/* {
-            place.images && place.images.map((item) => (
+        </div>
+        <div className={styles.product__image}>
+            {
+            place.place.images && place.place.images.map((item) => (
             <img src={item} key={item}/>
             ))
-            } */}
+            }
         </div>
         <div className={styles.product__description}>
-          <div className={styles.product__left}>
-            <div className={styles.product__header}>
-              <h2>{place.types}</h2>
-              <h2>Chez {place.owner}</h2>
+          <div className={styles.product__description__left}>
+            <div className={styles.product__description__top}>
+              <h2>Type : {place.typePlace.name}</h2>
+              <h2>Chez : {place.user.firstName} {place.user.lastName} </h2>
             </div>  
-            <div className={styles.product__info}>
+            <div className={styles.product__description__info}>
               <ul className={styles.nav__list}>
-                <li className={styles.nav__item}>{place.capacity}</li>
+                <li className={styles.nav__item}>{place.place.capacity} voyageurs</li>
                 <li className={styles.nav__item}>2 chambres</li>
                 <li className={styles.nav__item}>2 lits</li>
                 <li className={styles.nav__item}>2 sallle de bain et 1 toilette</li>
               </ul>
             </div>
             <hr />
-            <div className={styles.product__detail}>
-               <div className={styles.detail2}>
+            <div className={styles.product__description__detail}>
+               <div className={styles.detail}>
                   <img src={Home.src} alt="home" />
-                  <p>
-                  Idéal pour le télétravail
-                  Une connexion wifi rapide à 418 Mbit/s, plus un espace de travail dans une chambre privée.
-                  </p>
+                  <div>
+                    <p className={styles.nav__decor}>Idéal pour le télétravail</p>
+                    <p>
+                    Une connexion wifi rapide à 418 Mbit/s, plus un espace de travail dans une chambre privée.
+                    </p>
+                  </div>
                </div>
-               <div className={styles.detail2}>
+               <div className={styles.detail}>
                   <img src={Door.src} alt="door" />
-                  <p>
-                  Arrivée autonome
-                  Vous pouvez entrer dans les lieux avec une boîte à clé sécurisée.
-                  </p>
+                  <div>
+                    <p className={styles.nav__decor}>Arrivée autonome</p>
+                    <p>
+                    Vous pouvez entrer dans les lieux avec une boîte à clé sécurisée.
+                    </p>
+                  </div>
                </div>
-               <div className={styles.detail2}>
+               <div className={styles.detail}>
                   <img src={Medal.src} alt="medal" />
-                  <p>
-                  {place.owner} est Superhôte
-                  Les Superhôtes sont des hôtes expérimentés qui bénéficient de très bonnes évaluations et qui s'engagent à offrir d'excellents séjours aux voyageurs.
-                  </p>
+                  <div>
+                    <p className={styles.nav__decor}>{place.user.lastName} est Superhôte</p>
+                    <p>
+                    Les Superhôtes sont des hôtes expérimentés qui bénéficient de très bonnes évaluations et qui s'engagent à offrir d'excellents séjours aux voyageurs.
+                    </p>
+                  </div>
                </div>
             </div>
+            <hr />
+            <div className={styles.product__description__airCover}>
+              <img src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" alt="" />
+              <p>Chaque réservation comprend une protection gratuite en cas d'annulation par l'hôte, d'inexactitudes dans la description du logement, ainsi que d'autres problèmes comme les difficultés d'accès au logement.</p>
+              <p className={styles.nav__decor}>En savoir plus</p>
+            </div>
           </div>
-          <div className={styles.product__righ}>
-            <div className={styles.product__reservation}>
-              <div className={styles.product__price}>{place.price} €</div>
+          <div className={styles.product__decription__right}>
+            <div className={styles.product__description__reservation}>
+              <div className={styles.product__price}>{place.place.price} € par nuit</div>
               <form className={styles.form__reservation}>
                 <Input
-                  titleLabel="From"
+                  titleLabel="Départ"
                   inputType="date"
                   inputPlaceholder="from"
                   inputName="from"
@@ -137,10 +140,20 @@ const Index = () => {
                   // }}
                 />
                 <Input
-                  titleLabel="To"
+                  titleLabel="Arrivée"
                   inputType="date"
                   inputPlaceholder="to"
                   inputName="to"
+                  // inputValue={userForm.password || ""}
+                  // inputOnChange={(e) => {
+                  //   handleInput(e);
+                  // }}
+                />
+                <Input
+                  titleLabel="Voyageurs"
+                  inputType="number"
+                  inputPlaceholder="quantity"
+                  inputName="quantity"
                   // inputValue={userForm.password || ""}
                   // inputOnChange={(e) => {
                   //   handleInput(e);
